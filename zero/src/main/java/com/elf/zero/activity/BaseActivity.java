68b54182ab9fb54d1d91202d829f3e3aff1d2049
shuijinglingsos <84719419@qq.com>
2017-11-07 16:05:26 +0800

    添加到源代码

---
+++ zero/src/main/java/com/elf/zero/activity/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.elf.zero.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * activity 基类
+ * Created by Lidong on 2017/11/7.
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+
+
+}
