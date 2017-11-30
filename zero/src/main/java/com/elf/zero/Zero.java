e900ed4537ce17d00f0424c6e41c79f7a3e56219
shuijinglingsos <84719419@qq.com>
2017-11-30 15:18:34 +0800

    添加到源代码

---
+++ zero/src/main/java/com/elf/zero/Zero.java
@@ -0,0 +1,23 @@
+package com.elf.zero;
+
+import android.content.Context;
+
+import com.elf.zero.utils.AppUtils;
+import com.elf.zero.utils.DeviceUtils;
+
+/**
+ * zero
+ * Created by Lidong on 2017/11/30.
+ */
+public class Zero {
+
+    /**
+     * 初始化
+     *
+     * @param context application context
+     */
+    public static void init(Context context) {
+        AppUtils.init(context);
+        DeviceUtils.init(context);
+    }
+}
