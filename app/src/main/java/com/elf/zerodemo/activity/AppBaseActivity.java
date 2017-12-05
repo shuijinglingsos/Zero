a366b9b7b59cbab41287c8a7d347733655b3fec9
shuijinglingsos <84719419@qq.com>
2017-12-05 12:23:39 +0800

    新增showToast方法

--- app/src/main/java/com/elf/zerodemo/activity/AppBaseActivity.java
+++ app/src/main/java/com/elf/zerodemo/activity/AppBaseActivity.java
@@ -2,6 +2,7 @@ package com.elf.zerodemo.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class AppBaseActivity extends AppCompatActivity {
 
@@ -12,5 +13,9 @@ public class AppBaseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setTitle(getIntent().getStringExtra(ARG_TITLE));
+    }
+
+    protected void showToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
