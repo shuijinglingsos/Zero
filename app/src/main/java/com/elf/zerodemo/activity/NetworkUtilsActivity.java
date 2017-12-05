130995abdf2e8e563da20c3b848563c6279ad202
shuijinglingsos <84719419@qq.com>
2017-12-05 12:25:08 +0800

    增加NewworkUtils测试

--- app/src/main/java/com/elf/zerodemo/activity/NetworkUtilsActivity.java
+++ app/src/main/java/com/elf/zerodemo/activity/NetworkUtilsActivity.java
@@ -1,7 +1,9 @@
 package com.elf.zerodemo.activity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.elf.zero.utils.NetworkUtils;
 import com.elf.zerodemo.R;
 
 public class NetworkUtilsActivity extends AppBaseActivity {
@@ -10,5 +12,13 @@ public class NetworkUtilsActivity extends AppBaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_network_utils);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("是否有网络连接：").append(NetworkUtils.isNetworkConnection());
+        sb.append("\n是否是wifi网络：").append(NetworkUtils.isWifiConnection());
+        sb.append("\n是否是移动网络：").append(NetworkUtils.isMobileConnection());
+        sb.append("\n当前网络类型：").append(NetworkUtils.getNetworkType());
+
+        ((TextView) findViewById(R.id.textView)).setText(sb.toString());
     }
 }
