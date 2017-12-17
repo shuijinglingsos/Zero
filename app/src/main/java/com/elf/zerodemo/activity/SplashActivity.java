a5f37b26224cc6ad5de7bfedbd679bc2c27f8453
lirundong <84719419@qq.com>
2017-12-17 21:44:49 +0800

    闪屏页全屏 透明状态栏页测试

--- app/src/main/java/com/elf/zerodemo/activity/SplashActivity.java
+++ app/src/main/java/com/elf/zerodemo/activity/SplashActivity.java
@@ -1,11 +1,13 @@
 package com.elf.zerodemo.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.elf.zerodemo.R;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppBaseActivity {
 
@@ -17,9 +19,26 @@ public class SplashActivity extends AppBaseActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                transparentStatus();
                 startActivity(new Intent(SplashActivity.this,MainActivity.class));
                 finish();
             }
         },1000);
+    }
+
+    private void transparentStatus(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+//                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                     View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+//            window.setNavigationBarColor(Color.TRANSPARENT);
+        }
     }
 }
--- app/src/main/java/com/elf/zerodemo/activity/TranslucentStatusActivity.java
+++ app/src/main/java/com/elf/zerodemo/activity/TranslucentStatusActivity.java
@@ -1,13 +1,13 @@
 package com.elf.zerodemo.activity;
 
-import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-import com.elf.zero.activity.BaseActivity;
 import com.elf.zerodemo.R;
 
 public class TranslucentStatusActivity extends AppBaseActivity {
@@ -17,7 +17,12 @@ public class TranslucentStatusActivity extends AppBaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_translucent_status);
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(getIntent().getStringExtra(ARG_TITLE));
+
+        ViewCompat.setElevation(toolbar, 8);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
 //            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
 //                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
@@ -26,7 +31,7 @@ public class TranslucentStatusActivity extends AppBaseActivity {
 //                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
             );
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.TRANSPARENT);
+            window.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
 //            window.setNavigationBarColor(Color.TRANSPARENT);
         }
     }
