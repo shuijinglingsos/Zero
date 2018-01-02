88f8ddef7e69703d7d41a8324ac1637e5a86bbc8
shuijinglingsos <84719419@qq.com>
2018-01-02 14:51:06 +0800

    增加图片压缩

---
+++ zero/src/main/java/com/elf/zero/image/ImageCompress.java
@@ -0,0 +1,29 @@
+package com.elf.zero.image;
+
+import android.content.Context;
+
+import java.io.File;
+import java.io.IOException;
+
+import top.zibin.luban.Luban;
+
+/**
+ * 图片压缩
+ * Created by Lidong on 2018/1/2.
+ */
+public class ImageCompress {
+
+    private Context mContext;
+
+    ImageCompress(Context context) {
+        mContext = context;
+    }
+
+    public File compress(File srcImg) throws IOException {
+        return Luban.with(mContext).load(srcImg).get().get(0);
+    }
+
+    public static ImageCompress Build(Context context) {
+        return new ImageCompress(context);
+    }
+}
