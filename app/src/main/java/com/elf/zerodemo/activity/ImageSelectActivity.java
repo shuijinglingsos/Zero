889c663b8939add05f886df48aaa1c2b4ccc2b4a
lirundong <84719419@qq.com>
2018-01-03 21:45:08 +0800

    添加获取图片代码

--- app/src/main/java/com/elf/zerodemo/activity/ImageSelectActivity.java
+++ app/src/main/java/com/elf/zerodemo/activity/ImageSelectActivity.java
@@ -1,15 +1,32 @@
 package com.elf.zerodemo.activity;
 
+import android.Manifest;
 import android.content.ContentResolver;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.provider.MediaStore;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.elf.zerodemo.R;
+import com.elf.zerodemo.model.ImageDetail;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -17,29 +34,137 @@ import java.util.Map;
  */
 public class ImageSelectActivity extends AppCompatActivity {
 
+    private AbsListView mGridView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_select);
+        mGridView = (AbsListView) findViewById(R.id.gridView);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            String[] permission = {Manifest.permission.READ_EXTERNAL_STORAGE};
+            ActivityCompat.requestPermissions(this, permission, 1);
+            return;
+        }
+
+        showData();
     }
 
-//    /**
-//     * Image attribute.
-//     */
-//    private static final String[] IMAGES = {
-//            MediaStore.Images.Media.DATA,
-//            MediaStore.Images.Media.DISPLAY_NAME,
-//            MediaStore.Images.Media.TITLE,
-//            MediaStore.Images.Media.BUCKET_ID,
-//            MediaStore.Images.Media.BUCKET_DISPLAY_NAME,
-//            MediaStore.Images.Media.MIME_TYPE,
-//            MediaStore.Images.Media.DATE_ADDED,
-//            MediaStore.Images.Media.DATE_MODIFIED,
-//            MediaStore.Images.Media.LATITUDE,
-//            MediaStore.Images.Media.LONGITUDE,
-//            MediaStore.Images.Media.SIZE
-//    };
-//
+    private void showData(){
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this,"没有权限",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        List<ImageDetail> list = new ArrayList<>();
+
+        ContentResolver contentResolver = getContentResolver();
+        Cursor cursor = contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                IMAGES,
+                null,
+                null,
+                MediaStore.Images.Media.DATE_ADDED);
+
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                String path = cursor.getString(cursor.getColumnIndex(IMAGES[0]));
+
+                File file = new File(path);
+                if (!file.exists() || !file.canRead()) continue;
+
+                ImageDetail imageDetail = new ImageDetail();
+                imageDetail.path = path;
+                imageDetail.name = cursor.getString(cursor.getColumnIndex(IMAGES[1]));
+                imageDetail.title = cursor.getString(cursor.getColumnIndex(IMAGES[2]));
+                imageDetail.bucketId = cursor.getInt(cursor.getColumnIndex(IMAGES[3]));
+                imageDetail.bucketName = cursor.getString(cursor.getColumnIndex(IMAGES[4]));
+                imageDetail.mimeType = cursor.getString(cursor.getColumnIndex(IMAGES[5]));
+                imageDetail.addDate = cursor.getLong(cursor.getColumnIndex(IMAGES[6]));
+                imageDetail.modifyDate = cursor.getLong(cursor.getColumnIndex(IMAGES[7]));
+                imageDetail.latitude = cursor.getFloat(cursor.getColumnIndex(IMAGES[8]));
+                imageDetail.longitude = cursor.getFloat(cursor.getColumnIndex(IMAGES[9]));
+                imageDetail.size = cursor.getLong(cursor.getColumnIndex(IMAGES[10]));
+                imageDetail.id = cursor.getLong(cursor.getColumnIndex(IMAGES[11]));
+
+                Cursor cursor1 = getContentResolver().query(MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI
+                        , null
+                        , MediaStore.Images.Thumbnails.IMAGE_ID + "=?"
+                        , new String[]{imageDetail.id + ""}
+                        , null);
+
+                if (cursor1 != null && cursor1.moveToFirst()) {
+                    imageDetail.fmPaht = cursor1.getString(cursor.getColumnIndex(MediaStore.Images.Thumbnails.DATA));
+                }
+
+                list.add(imageDetail);
+            }
+            cursor.close();
+        }
+
+        ImageGridAdapter adapter = new ImageGridAdapter(this);
+        adapter.addAll(list);
+        mGridView.setAdapter(adapter);
+    }
+
+    private class ImageGridAdapter extends ArrayAdapter<ImageDetail> {
+
+        public ImageGridAdapter(@NonNull Context context) {
+            super(context, 0);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+            if (convertView == null) {
+                convertView = new TextView(getContext());
+            }
+            ImageDetail detail = getItem(position);
+            StringBuilder sb = new StringBuilder();
+            sb.append("id：" + detail.id).append("\n")
+                    .append("名称：" + detail.name).append("(").append(detail.mimeType).append(")").append("\n")
+                    .append("title：" + detail.title).append("\n")
+                    .append("添加时间：" + detail.addDate).append("\n")
+                    .append("修改时间：" + detail.modifyDate).append("\n")
+                    .append("文件夹:" + detail.bucketName).append("(").append(detail.bucketId).append(")").append("\n")
+                    .append("路径：").append(detail.path).append("\n")
+                    .append("封面：").append(detail.fmPaht);
+
+            ((TextView) convertView).setText(sb.toString());
+
+            return convertView;
+        }
+    }
+
+    /**
+     * Image attribute.
+     */
+    private static final String[] IMAGES = {
+            MediaStore.Images.Media.DATA,
+            MediaStore.Images.Media.DISPLAY_NAME,
+            MediaStore.Images.Media.TITLE,
+            MediaStore.Images.Media.BUCKET_ID,
+            MediaStore.Images.Media.BUCKET_DISPLAY_NAME,
+            MediaStore.Images.Media.MIME_TYPE,
+            MediaStore.Images.Media.DATE_ADDED,
+            MediaStore.Images.Media.DATE_MODIFIED,
+            MediaStore.Images.Media.LATITUDE,
+            MediaStore.Images.Media.LONGITUDE,
+            MediaStore.Images.Media.SIZE,
+            MediaStore.Images.Media._ID
+    };
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        showData();
+    }
+
 //    /**
 //     * Video attribute.
 //     */
---
+++ app/src/main/java/com/elf/zerodemo/model/ImageDetail.java
@@ -0,0 +1,22 @@
+package com.elf.zerodemo.model;
+
+/**
+ * 图片详情
+ * Created by Lidong on 2018/1/3.
+ */
+public class ImageDetail {
+
+    public long id;
+    public String path;
+    public String name;
+    public String title;
+    public int bucketId;
+    public String bucketName;
+    public String mimeType;
+    public long addDate;
+    public long modifyDate;
+    public float latitude;
+    public float longitude;
+    public long size;
+    public String fmPaht;
+}
