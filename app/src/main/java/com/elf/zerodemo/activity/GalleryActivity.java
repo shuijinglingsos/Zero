bce00879761b4603b675c89eb2fbf1174af86943
shuijinglingsos <84719419@qq.com>
2018-02-26 14:06:20 +0800

    优化显示效果

--- app/src/main/java/com/elf/zerodemo/activity/GalleryActivity.java
+++ app/src/main/java/com/elf/zerodemo/activity/GalleryActivity.java
@@ -133,7 +133,7 @@ public class GalleryActivity extends AppBaseActivity {
 //            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
 //                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
             window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                     View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 //                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
             );
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
