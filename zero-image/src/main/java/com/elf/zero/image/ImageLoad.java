a8874706008eda63b99db65a248eb90e79817d11
shuijinglingsos <shuijinglingsos@163.com>
2018-08-08 16:06:24 +0800

    重构image组件

---
+++ zero-image/src/main/java/com/elf/zero/image/ImageLoad.java
@@ -0,0 +1,9 @@
+package com.elf.zero.image;
+
+/**
+ * 图片加载
+ * Created by Lidong on 2018/6/30.
+ */
+
+public class ImageLoad {
+}
---
+++ zero-image/src/main/java/com/elf/zero/image/ImageLoadInterface.java
@@ -0,0 +1,10 @@
+package com.elf.zero.image;
+
+/**
+ * 图片加载接口
+ * Created by Lidong on 2018/6/30.
+ */
+
+public interface ImageLoadInterface {
+
+}
